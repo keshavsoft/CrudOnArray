ac0f4f60b4aa9f761d2541432e6e1a97f3de51b9
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-10-25 12:43:22 +0530

    BulkUpdateByPk

--- src/V17/GenerateWMail/CommonCode/RestFiles/Import/entryFile.js
+++ src/V17/GenerateWMail/CommonCode/RestFiles/Import/entryFile.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const { StartFunc: StartFuncFromAsIs } = require('./AsIs');
+const { StartFunc: StartFuncFromBulkUpdateByPk } = require('./BulkUpdateByPk');
 
 const handlers = {
-    "1_AsIs.http": StartFuncFromAsIs
+    "1_AsIs.http": StartFuncFromAsIs,
+    "2_BulkUpdateByPk.http": StartFuncFromBulkUpdateByPk
 };
 
 async function StartFunc({ inFolder, inTableName, inVersion, inPortNumber, inColumnsAsArray }) {
--- src/V17/GenerateWMail/TableName/Import/routes.js
+++ src/V17/GenerateWMail/TableName/Import/routes.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 import { router as routerFromAsIs } from "./1.AsIs/routes.js";
-import { router as routerFromBulkUpdateByPk } from "./3.BulkUpdateByPk/routes.js";
+import { router as routerFromBulkUpdateByPk } from "./2.BulkUpdateByPk/routes.js";
 
 router.use("/AsIs", routerFromAsIs);
 router.use("/BulkUpdateByPk", routerFromBulkUpdateByPk);
