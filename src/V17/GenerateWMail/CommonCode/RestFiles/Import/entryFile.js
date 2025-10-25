f8aa71592f27193fd1daa2fd42638c95daccae34
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-10-25 14:58:49 +0530

    BulkTransformByType

--- src/V17/GenerateWMail/CommonCode/RestFiles/Import/entryFile.js
+++ src/V17/GenerateWMail/CommonCode/RestFiles/Import/entryFile.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const { StartFunc: StartFuncFromAsIs } = require('./AsIs');
 const { StartFunc: StartFuncFromBulkUpdateByPk } = require('./BulkUpdateByPk');
+const { StartFunc: StartFuncFromBulkTransformByType } = require('./BulkTransformByType');
 
 const handlers = {
     "1_AsIs.http": StartFuncFromAsIs,
-    "2_BulkUpdateByPk.http": StartFuncFromBulkUpdateByPk
+    "2_BulkUpdateByPk.http": StartFuncFromBulkUpdateByPk,
+    "3_BulkTransformByType.http": StartFuncFromBulkTransformByType
 };
 
 async function StartFunc({ inFolder, inTableName, inVersion, inPortNumber, inColumnsAsArray }) {
--- src/V17/GenerateWMail/TableName/Import/routes.js
+++ src/V17/GenerateWMail/TableName/Import/routes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 import { router as routerFromAsIs } from "./1.AsIs/routes.js";
 import { router as routerFromBulkUpdateByPk } from "./2.BulkUpdateByPk/routes.js";
+import { router as routerFromBulkTransformByType } from "./3.BulkTransformByType/routes.js";
 
 router.use("/AsIs", routerFromAsIs);
 router.use("/BulkUpdateByPk", routerFromBulkUpdateByPk);
+router.use("/BulkTransformByType", routerFromBulkTransformByType);
 
 export { router };
