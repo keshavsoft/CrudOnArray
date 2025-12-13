d1ddad214bfc4e4a74249063fa8629e1358a2690
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-12-13 13:01:39 +0530

    GenWMailAndWs.OnlyBackEnd.Secured Started

--- src/V20/GenWMailAndWs/OnlyBackEnd/entryFile.js
+++ src/V20/GenWMailAndWs/OnlyBackEnd/entryFile.js
@@ -1,7 +1,9 @@
 const { StartFunc: StartFuncFromNonSecured } = require("./NonSecured/entryFile");
+const { StartFunc: StartFuncFromSecured } = require("./Secured/entryFile");
 
 const StartFunc = () => {
     StartFuncFromNonSecured();
+    StartFuncFromSecured();
 };
 
 module.exports = { StartFunc };
